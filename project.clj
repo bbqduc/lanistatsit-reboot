721c01af76185ef9203223fea128f47f1832695a
bbqduc <brunoankka@gmail.com>
2016-06-30 11:14:30 +0300

    Remove .css from clean-targets since it's handwritten now

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 
   :source-paths ["src/clj"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target" "resources/public/css"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
