c53b0f2711ae978ba58a55699a225b31799ec927
bbqduc <brunoankka@gmail.com>
2016-07-04 13:41:23 +0300

    Alter react id logic so it doesn't change on every render

--- src/cljs/lanistatsit/views.cljs
+++ src/cljs/lanistatsit/views.cljs
@@ -2,19 +2,18 @@
   (:require [re-frame.core :as re-frame]
             [goog.string :as gstring]
             [goog.string.format]
-            [lanistatsit.key :refer [gen-key]]
             ))
 
 (defonce lans [{:lan "Lan 1", :wins 10, :losses 8}
                {:lan "Lan 2", :wins 15, :losses 11}])
 
-(defn sortable-table-row [data data-keys]
-  ^{:key (gen-key)} [:tr
+(defn sortable-table-row [data data-keys id]
+  ^{:key id} [:tr
    (for [data-key data-keys]
      (let [trans (:transform data-key)
            value (get data (:key data-key))
            content (if (nil? trans) value (trans value))]
-       ^{:key (gen-key)} [:td content]))])
+       ^{:key (name (:key data-key))} [:td content]))])
 
 (defn sortable-table-header-cell [table-id data-key]
   ^{:key (name data-key)} [:th
@@ -29,8 +28,8 @@
         (sortable-table-header-cell table-id data-key))]
      [:tbody
       (let [data-sub (re-frame/subscribe [:table-data data-id table-id])]
-        (for [data @data-sub]
-          (sortable-table-row data data-keys)))]]))
+        (map-indexed #(sortable-table-row %2 data-keys %1) @data-sub)
+        )]]))
 
 (defn percentage-string [percentage]
   (str (* 100 percentage) "%"))
@@ -39,11 +38,11 @@
   (let [lan (:lan stats)
         wins (:wins stats)
         losses (:losses stats)]
-    [:div {:class "lanlistentry" :key (gen-key)}
-     [:a {:href (str "/lans/" lan) :key (gen-key)} lan]
-     [:ul {:class "lanlist" :key (gen-key)}
-      [:li {:key (gen-key)} (gstring/format "Winrate: %.2f%" (percentage-string (/ wins (+ wins losses))))]
-      [:li {:key (gen-key)} (str wins "/" losses)]]]))
+    [:div {:class "lanlistentry" :key lan}
+     [:a {:href (str "/lans/" lan) :key (str "a_" lan)} lan]
+     [:ul {:class "lanlist" :key (str "ul_" lan)}
+      [:li {:key "winrate"} (gstring/format "Winrate: %.2f%" (percentage-string (/ wins (+ wins losses))))]
+      [:li {:key "winslosses"} (str wins "/" losses)]]]))
 
 (defn lan-list [lans]
   (fn []
